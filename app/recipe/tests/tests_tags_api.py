4b598a9629fc9faafafdf2cb569770cf0776195a
manishmishra650 <manishmishra650@gmail.com>
2020-06-16 19:01:05 +0530

    changes for tag cteation

--- app/recipe/tests/tests_tags_api.py
+++ app/recipe/tests/tests_tags_api.py
@@ -61,3 +61,21 @@ class PrivateTagsApiTest(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], tag.name)
+
+    def test_create_tag_successfull(self):
+        """create a tag and test the vaidation"""
+        payload = {'name': 'test tag'}
+        self.client.post(TAG_URL, payload)
+
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """create tag with invalid data"""
+        payload = {'name': ''}
+        res = self.client.post(TAG_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
