8de3dccf0c4515fecfa20ddbb184b17d561c33bd
manishmishra650 <manishmishra650@gmail.com>
2020-06-17 13:45:09 +0530

    changes for adding Ingredients

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -52,3 +52,12 @@ class ModelTests(TestCase):
             name='Vegan'
         )
         self.assertEqual(str(tag), tag.name)
+
+    def test_ingredient_str(self):
+        """test the ingrdient string reprasentation"""
+        ingredient = models.Ingredient.objects.create(
+            user=sample_user(),
+            name='Cucumber'
+        )
+
+        self.assertEqual(str(ingredient), ingredient.name)
